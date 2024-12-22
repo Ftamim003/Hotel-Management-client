81ab3f2b7d37f19365fdd9464544b3fb6829752f
ftamim003 <fazlullahtamim@gmail.com>
2024-12-22 22:18:28 +0600

    basic setup done

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
+
+const MainLayout = () => {
+    return (
+        <div className="">
+        <Navbar></Navbar>
+        <div className="min-h-[calc(100vh-200px)] md:min-h-[calc(100vh-230px)] w-11/12 mx-auto">
+
+            <Outlet></Outlet>
+        </div>
+        <Footer></Footer>
+    </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Home from "../Home/Home";
+import MainLayout from "../MainLayout/MainLayout";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+      ]
+    },
+  ]);
+
+  export default router
